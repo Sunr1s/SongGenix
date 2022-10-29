3e102dcda87635208cc7d0d3b1000a6abf824f34
Ustenko Volodymyr <koldon6@gmail.com>
2022-10-29 12:33:29 +0300

    initial styles and gitignore changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client'
-const socket = io.connect("http://localhost:5000")
+import React from 'react';
 
 function App() {
 
