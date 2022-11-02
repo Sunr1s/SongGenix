c753109c94783c2a1a4eb5a0493156e6e43810d5
Sunr1se <david_gosha@knu.ua>
2022-11-02 23:49:25 +0200

    added dell

--- server/app.py
+++ server/app.py
@@ -1,29 +1,28 @@
-import random,uuid, pymongo,flask
-from flask import jsonify
-from pymongo import MongoClient
-from turtle import title
+from bson.objectid import ObjectId
 from urllib import request
-from flask import Flask, make_response, url_for, request,flash,session,redirect,abort
-from flask_socketio import SocketIO, join_room, leave_room, send
-from flask_session import Session
+from pymongo import MongoClient
+from flask import Flask, make_response, request
 
 app = Flask(__name__)
-
 client = MongoClient('localhost', 27017)
 db = client.SongGenix
-
 
 @app.route('/createLobby', methods=["POST"])
 def createLobby():
     if len(request.json['username']) > 2:
         lobby = {
-            "admin":request.json['username'],
+            "admin": request.json['username'],
             "users": [],
             "settings": {}
         }
         post_id = db.Lobby.insert_one(lobby).inserted_id
         lobby["_id"] = str(post_id)
     return make_response(lobby, 200)
+
+@app.route('/deleteRoom/<id>', methods=['DELETE'])
+def deleteRoom(id):
+    result = db.Lobby.delete_one({'_id': ObjectId(id)})
+    return make_response("ok", 200)
 
 if __name__ == "__main__":
     app.run()
