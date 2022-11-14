d6a805403640c4c5d1ec4897571da0e15982ef15
kiri <oachkevych@gmail.com>
2022-11-14 10:23:25 +0200

    design to firt page with questions

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import './App.scss';
 function App() {
 
-    return (
-      <div className="App">123</div>
-    );
-  }
+  return (
+    <div className="App">
+      <div>
+        <div className="white-oval">Готові перевірити свої музичні знання?</div>
+        <div className='buttons'>
+          <div className='create-room-btn btn'>Створити кімнату</div>
+          <div className='connect-btn btn'>Приєднатись</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default App;
