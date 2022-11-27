5d69227f2726018e0525453374280fe01ef2535a
ste1nerr <oachkevych@gmail.com>
2022-11-27 21:42:41 +0200

    added options and members

--- client/src/Components/Room.jsx
+++ client/src/Components/Room.jsx
@@ -8,17 +8,54 @@ const Room = () => {
             <div className='App'>
                 <div className="room">
 
-                    <div className="left-column-room">
-                        <p className="">Учасники</p>
+                    <div className="left-column-room column-room">
+                        <p className="title-room">Учасники</p>
+                        <div className="cards">
+                            <div className='person-card'>
+                                <p className="person-card-place">1.</p>
+                                <p className="person-card-name">Alex Kiri -</p>
+                                <p className="person-card-points">333</p>
+                            </div>
+                            <div className='person-card'>
+                                <p className="person-card-place">2.</p>
+                                <p className="person-card-name">Vova -</p>
+                                <p className="person-card-points">313</p>
+                            </div>
+                            <div className='person-card'>
+                                <p className="person-card-place">3.</p>
+                                <p className="person-card-name">SunR1se -</p>
+                                <p className="person-card-points">299</p>
+                            </div>
+                        </div>
+
                     </div>
 
                     <div className="center-column-room">
-
+                        <div className='add-playlist-btn btn'><p className="">Додати свій плейлист</p></div>
+                        <div className='choose-playlist-btn btn'><p className="">Обрати плейлист</p></div>
                     </div>
 
 
-                    <div className="right-column-room">
-                        Налаштування
+                    <div className="right-column-room column-room">
+                        <p className="title-room">Налаштування</p>
+                        <div className="room-options">
+                            <div className="room-line"></div>
+                            <p className="room-options-title">Кількість треків</p>
+                            <div className="roop-options-quantity-tracks">
+                                <div className='quantity-tracks-row'>
+                                    <p>5</p>
+                                    <p>10</p>
+                                    <p>15</p>
+                                </div>
+
+                                <div className='quantity-tracks-row'>
+                                    <p>25</p>
+                                    <p>30</p>
+                                    <p>50</p>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
