a260690baf19970b132658375fad52e872c9a725
ste1nerr <oachkevych@gmail.com>
2022-11-27 15:32:16 +0200

    styles to room page

--- client/src/Components/Room.jsx
+++ client/src/Components/Room.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import '../App.scss';
 
 const Room = () => {
 
     return (
         <div>
-            ROOM
+            <div className='App'>
+                <div className="room">
+
+                    <div className="left-column-room">
+                        <p className="">Учасники</p>
+                    </div>
+
+                    <div className="center-column-room">
+
+                    </div>
+
+
+                    <div className="right-column-room">
+                        Налаштування
+                    </div>
+                </div>
+
+            </div>
         </div>
+
     )
 }
 
